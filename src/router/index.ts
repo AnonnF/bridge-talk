de485aa9d6d546eca4ffb2cf5c77b0055b5e70b5
Zhan <bs2124@ic.ac.uk>
2026-05-28 17:03:29 +0100

    feat: align scenario quiz page with Figma design and link to question bank

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,11 +1,17 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomePage from '../views/HomePage.vue'
 import PracticePage from '../views/PracticePage.vue'
+import ScenarioQuizPage from '../views/ScenarioQuizPage.vue'
 
 export const router = createRouter({
   history: createWebHistory(),
   routes: [
     { path: '/', name: 'home', component: HomePage },
     { path: '/practice', name: 'practice', component: PracticePage },
+    {
+      path: '/practice/:scenarioId',
+      name: 'scenario-quiz',
+      component: ScenarioQuizPage,
+    },
   ],
 })
